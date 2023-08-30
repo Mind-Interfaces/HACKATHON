47bce81266ca0168406626f9e1cc154e66ac18bf
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-08-30 07:21:02 -0400

    Create AudioSurf-3D.py

---
+++ AudioSurf-3D.py
@@ -0,0 +1,118 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import random
+
+# Initialize Pygame and OpenGL
+pygame.init()
+pygame.mixer.init()  # Initialize the mixer module for sound
+pygame.mixer.music.load("pong.wav")  # Load the pong.wav sound
+display = (1920, 1080)
+pygame.display.set_mode(display, DOUBLEBUF | OPENGL | pygame.RESIZABLE)
+gluPerspective(30, (display[0] / display[1]), 1, 50.0)
+glTranslatef(0.0, 0.0, -10)
+
+# Apply tilt
+tilt = 30
+glPushMatrix()
+glRotatef(tilt, 10, 0, 0)
+# glPopMatrix()
+
+# Global Variables
+player_x = 0
+player_y = -1.5
+obstacles = []
+collided_ids = set()
+collision_count = 0
+obstacle_id = 0
+
+# Function to draw a cube
+def draw_cube(x, y, z, size, color=(1, 1, 1)):
+    vertices = [
+        [size + x, -size + y, -size + z],
+        [size + x, size + y, -size + z],
+        [-size + x, size + y, -size + z],
+        [-size + x, -size + y, -size + z],
+        [size + x, -size + y, size + z],
+        [size + x, size + y, size + z],
+        [-size + x, size + y, size + z],
+        [-size + x, -size + y, size + z]
+    ]
+    edges = [
+        (0, 1), (1, 2), (2, 3), (3, 0),
+        (4, 5), (5, 6), (6, 7), (7, 4),
+        (0, 4), (1, 5), (2, 6), (3, 7)
+    ]
+    glBegin(GL_LINES)
+    glColor3f(*color)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+# Function to detect collision between player and obstacle
+def is_collision(player_x, player_y, player_z, obstacle_x, obstacle_y, obstacle_z, threshold=0.4):
+    return abs(player_x - obstacle_x) < threshold and abs(player_y - obstacle_y) < threshold and abs(player_z - obstacle_z) < threshold
+
+def draw_text(text, x, y, color=(0, 0, 0)):
+    font = pygame.font.SysFont("Arial", 12)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.topright = (x, y)
+    # pygame.display.get_active().blit(text_surface, text_rect)
+
+# Main Function
+def main():
+    global player_x, player_y, obstacles, collision_count, obstacle_id
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player_x -= 0.1
+        if keys[pygame.K_d]:
+            player_x += 0.1
+        if keys[pygame.K_w]:
+            player_y += 0.1
+        if keys[pygame.K_s]:
+            player_y -= 0.1
+
+        if random.choice([True, False]):
+            obstacles.append([str(obstacle_id), random.uniform(-4, 4), -1.5, -30, 0.2, (0, 1, 0)])
+            obstacle_id += 1  # Increment the obstacle ID
+
+        new_obstacles = []
+        for obs_id, x, y, z, s, color in obstacles:
+            z += 0.1
+            if z < 5:
+
+                if is_collision(player_x, player_y, 0, x, y, z):
+                    if obs_id not in collided_ids:
+                        color = (0.5, 0.5, 0.5)  # Change color to grey upon collision
+                        pygame.mixer.music.play()
+                        collision_count += 1
+                    collided_ids.add(obs_id)  # Add the collided obstacle ID to the set
+                    # Draw the collision count
+                    draw_text("Collisions: " + str(collision_count), display[0] - 30, 10)
+                new_obstacles.append([obs_id, x, y, z, s, color])  # Update with the new color
+
+        obstacles = new_obstacles
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_cube(player_x, player_y, 0, 0.2, color=(1, 1, 1))  # Player cube is white
+
+        # Draw Obstacles and their IDs
+        for id, x, y, z, s, color in obstacles:
+            draw_cube(x, y, z, s, color)
+
+        # Update the display
+        pygame.display.flip()
+        clock.tick(30)
+
+main()
